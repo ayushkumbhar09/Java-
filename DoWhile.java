19a81c58b7cbb1b0a02184e6dc31d7d5e8f3ddaa
ayushkumbhar09 <ayushkumbhar1519@gmail.com>
2024-01-29 20:00:23 +0530

    First commit

---
+++ DoWhile.java
@@ -0,0 +1,17 @@
+package JAVA_A;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Do While loop program
+		int i =1;
+		do
+		{
+			System.out.println("hello" + i);
+			i++;
+		}while(i<=10);
+
+	}
+
+}
