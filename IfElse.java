fbba61e2026cf4115d8b245f8d0028f4aca2c9a2
ayushkumbhar09 <ayushkumbhar1519@gmail.com>
2024-01-30 10:09:46 +0530

    commmit done

---
+++ IfElse.java
@@ -0,0 +1,20 @@
+package JAVA_A;
+import java.util.*;
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter i");
+		int i = sc.nextInt();
+		 
+        if (i < 15) {
+            System.out.println(i+" is less than 15");
+        }
+        else {
+        System.out.println(i+" is greater than 15");
+        }
+	}
+
+}
