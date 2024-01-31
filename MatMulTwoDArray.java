75b19bd04c549e78fbaa1ff7cb87fbada843f4b1
ayushkumbhar09 <ayushkumbhar1519@gmail.com>
2024-01-31 15:51:25 +0530

    Array

---
+++ MatMulTwoDArray.java
@@ -0,0 +1,33 @@
+package JAVA_A;
+
+public class MatMulTwoDArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] a= {{1,2},{3,4}};
+		int[][] b= {{1,2},{3,4}};
+		int[][] c=new int[2][2];
+		for(int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				c[i][j]=0;
+				for(int k=0;k<2;k++)
+				{
+					c[i][j]=a[i][k]*b[k][j]+c[i][j];
+				}
+			}
+		}
+		for(int i=0;i<2;i++)
+		{
+			for(int j=0;j<2;j++)
+			{
+				System.out.print(c[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+
+	}
+
+
---
+++ OneDArray.java
@@ -0,0 +1,32 @@
+package JAVA_A;
+import java.util.*;
+public class OneDArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int marks[]= {30,23,40,45,10};
+	      int score[]=new int[5];
+	    /*  for(int i=0;i<5;i++)
+	      {
+	    	  System.out.println(marks[i]);
+	      }
+	      System.out.println("Enhanced for");
+	      for(int x:marks)
+	      {
+	    	  System.out.println(x);
+	      }*/
+	      Scanner sc=new Scanner(System.in);
+	      for(int i=0;i<5;i++)
+	      {
+	    	  System.out.println("Enter position "+(i+1)+" value");
+	    	  score[i]=sc.nextInt();
+	      }
+	      for(int i=0;i<5;i++)
+	      {
+	    	  System.out.println(score[i]);
+	      }
+		}
+
+	}
+
+
