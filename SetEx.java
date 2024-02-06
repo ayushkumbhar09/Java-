3feb2253c3d2e1782da1f4ef0b4a49684b660cc0
ayushkumbhar09 <ayushkumbhar1519@gmail.com>
2024-02-06 10:28:22 +0530

    que set

---
+++ SetEx.java
@@ -0,0 +1,26 @@
+package JAVA_A;
+import java.util.*;
+public class SetEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Set<String> s=new HashSet<>();
+		s.add("Ayush");
+		s.add("Virat");
+		s.add("jashshwi");
+		s.add("shubman");
+		s.add("tilak");
+		s.add("rohit");
+		System.out.println(s);
+	SortedSet<String> s1=new TreeSet<>();
+		s1.add("virat");
+		s1.add("suryansh");
+		s1.add("shivansh");
+		s1.add("mukesh");
+		s1.add("pankaj");
+		s1.add("Prakash");
+		System.out.println(s1);
+
+	}
+
+}
